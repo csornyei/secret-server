a431c06d1c2ee070d62a4833aa13eb560e0f66fd
matecsornyei <mate@csornyei.com>
2021-07-31 19:07:40 +0200

    Adding cypress for e2e testing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const createApp = require("./app");
+const { MongoMemoryServer } = require("mongodb-memory-server");
 
 const {
     MONGO_USER,
     MONGO_PASSWORD,
     SERVER_PORT,
-    CRYPTO_SECRET_KEY
+    CRYPTO_SECRET_KEY,
+    NODE_ENV
 } = process.env;
 
 const start = async () => {
@@ -14,7 +16,15 @@ const start = async () => {
         throw new Error('CRYPTO_SECRET_KEY must be defined!');
     }
     try {
-        await mongoose.connect(`${MONGO_USER}:${MONGO_PASSWORD}@mongodb://mongo:27017`, {
+        let mongodbUri;
+        if (NODE_ENV === "test") {
+            const mongodb = await MongoMemoryServer.create();
+
+            mongodbUri = mongodb.getUri();
+        } else {
+            mongodbUri = `${MONGO_USER}:${MONGO_PASSWORD}@mongodb://mongo:27017`
+        }
+        await mongoose.connect(mongodbUri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
